5fbdcf6e688ee7d98baab7772b25a4e1d0a7fe9a
U10416020 <wkps19961015@gmail.com>
2016-03-10 23:42:10 +0800

    Finish the main class
    Add if-else statements to judge the command line whether user entered a month, year, or none.

--- TestDisplayCalendars.java
+++ TestDisplayCalendars.java
@@ -5,8 +5,24 @@ public class TestDisplayCalendars {
 	//Main method
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int month = input.nextInt();
-		int year = intput.nextInt();
+		int month, year;
+		
+		//Create a object of GregorianCalendar
+		GregorianCalendar calendar = new GregorianCalendar();
+			
+		//Judge how much information does user input
+		if(args.length == 2){
+			month = Integer.parseInt(args[0]);
+			year = Integer.parseInt(args[1]);
+		}
+		else if (args.length == 1){
+			month = Integer.parseInt(args[0]);
+			year = calendar.get(Calendar.YEAR);
+		}
+		else{
+			month = calendar.get(Calendar.MONTH) + 1;
+			year = calendar.get(Calendar.YEAR);
+		}
 		
 		//Create a object of DisplayCalendars
 		DisplayCalendars test = new DisplayCalendars(month, year);
