efd52e2a340527c76f60001e6063527a7e9d1318
U10416020 <wkps19961015@gmail.com>
2016-03-10 23:33:45 +0800

    Create a class named TestDisplayCalendars
    The class to test DisplayCalendars class.

---
+++ TestDisplayCalendars.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+public class TestDisplayCalendars {
+	//Main method
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int month = input.nextInt();
+		int year = intput.nextInt();
+		
+		//Create a object of DisplayCalendars
+		DisplayCalendars test = new DisplayCalendars(month, year);
+	}
+}
