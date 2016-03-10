4850c1dff826853d91e85bb8bc02f99554c2aae5
U10416020 <wkps19961015@gmail.com>
2016-03-10 23:29:56 +0800

    Add some methods
    Add a method named printTitle to display the title of calendar.
    Add a Method named printBody to display the body of calendar.
    Add a accessor method to return the month with character.
    Add a accessor method to return the year.
    Add a accessor method to return the days of month.

--- DisplayCalendars.java
+++ DisplayCalendars.java
@@ -17,6 +17,56 @@ public class DisplayCalendars {
 	
 	//Method to display the calendar
 	public void printMonth(){
+		printTitle();
+		printBody();
 	}
 	
+	//Method to display the title of calendar
+	public void printTitle(){
+		System.out.println("\t\t" + getMonthName() + " " + getYear());
+		System.out.println("---------------------------------------------------");
+		System.out.println("Sun\tMon\tTue\tWed\tThu\tFri\tSat");
+	}
+	
+	//Method to display the body of calendar
+	public void printBody(){
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1;		
+		int i;
+		for(i = 0; i < dayOfWeek; i++){
+			System.out.print("\t");
+		}
+		for(i = 1; i <= getMonthDays(); i++){
+			System.out.print(i + "\t");
+			if((i + dayOfWeek) % 7 == 0){
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
+	//Accessor method to return the month with character
+	public String getMonthName(){
+		String[] monthName = {"January", "February", "March", "April",
+					"May","June", "July", "August", "September", 
+					"October", "November", "December"};
+		return monthName[month - 1];
+	}
+	
+	//Accessor method to return the year
+	public int getYear(){
+		return year;
+	}
+	
+	//Accessor method to return the days of month
+	public int getMonthDays(){
+		if(month == 1 || month == 3 || month == 5 || month == 7 || 
+		   month == 8 || month == 10 || month == 12)
+			return 31;
+		if(month == 4 || month == 6 || month == 9 || month == 11)
+			return 30;
+		if(month == 2 && calendar.isLeapYear(year))
+			return 29;
+		else 
+			return 28;
+	}
 }
